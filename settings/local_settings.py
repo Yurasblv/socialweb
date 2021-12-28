a0905d8e2feae80527362a78b7fac1af965c8fb9
yurasblv <yurasblv.y@gmail.com>
2021-12-28 17:12:08 +0200

    set settings

--- settings/local_settings.py
+++ settings/local_settings.py
@@ -50,12 +50,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'socialweb.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
