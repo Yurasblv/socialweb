a547ed50df5f26d585ce603c3a3cb140bbf82085
yurasblv <yurasblv.y@gmail.com>
2021-12-28 18:41:01 +0200

    set settings

--- settings/dev_settings.py
+++ settings/dev_settings.py
@@ -1,5 +1,4 @@
 import os
-
 from settings.local_settings import *
 
 
--- settings/prod_settings.py
+++ settings/prod_settings.py
@@ -1,5 +1,7 @@
 from settings.dev_settings import *
+import dj_database_url
 
+DATABASES['default']: dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 
 import django_heroku
 django_heroku.settings(locals())
