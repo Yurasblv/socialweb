465daf3e166245d17533c5228ab5828b973370f3
yurasblv <yurasblv.y@gmail.com>
2021-12-28 19:18:22 +0200

    set settings

--- settings/prod_settings.py
+++ settings/prod_settings.py
@@ -1,7 +1,9 @@
-from settings.dev_settings import *
+from settings.local_settings import *
 import dj_database_url
 import django_heroku
 
+DEBUG=False
+ALLOWED_HOSTS=['*']
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
